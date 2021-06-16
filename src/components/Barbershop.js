97c033269751336ea72e2820fabb6f6d0f3c9814
hrlnsnchz <hrlnsnchz@gmail.com>
2021-06-16 16:07:34 -0500

    set up basic npm React app with component folder and a temporary landing screen

---
+++ src/components/Barbershop.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Barbershop.css"
+
+export const Barbershop = () => (
+    <>
+        <h2>Luis' Barbershop</h2>
+        <address>
+            <div>Visit Us at the Nashville Location</div>
+            <div>300 Clean Cut Way</div>
+        </address>
+    </>
+)
